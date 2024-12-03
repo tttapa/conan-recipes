45a32fe67dea2b4cdbe65c6deb42ea0724d78be9
Pieter Pas <pieter.pas@outlook.com>
2024-12-04 00:49:19 +0100

    Fix CasADi builddirs on Windows

--- recipes/casadi/all/conanfile.py
+++ recipes/casadi/all/conanfile.py
@@ -254,4 +254,7 @@ class CasADiRecipe(ConanFile):
 
     def package_info(self):
         self.cpp_info.set_property("cmake_find_mode", "none")
-        self.cpp_info.builddirs.append(os.path.join("lib", "cmake", "casadi"))
+        if self.settings.compiler == "msvc":
+            self.cpp_info.builddirs.append(os.path.join("casadi", "cmake"))
+        else:
+            self.cpp_info.builddirs.append(os.path.join("lib", "cmake", "casadi"))
