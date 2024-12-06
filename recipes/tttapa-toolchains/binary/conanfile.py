5f1825be4dff449af06f00d257a11f46664fafde
Pieter Pas <pieter.p.dev@outlook.com>
2024-12-06 17:52:40 +0100

    Follow proper Conan tutorial for tttapa-toolchains https://docs.conan.io/2/examples/cross_build/toolchain_packages.html

--- recipes/tttapa-toolchains/binary/conanfile.py
+++ recipes/tttapa-toolchains/binary/conanfile.py
@@ -1,8 +1,9 @@
 import os
 
 from conan import ConanFile
-from conan.tools.files import get, copy
+from conan.tools.files import get
 from conan.errors import ConanInvalidConfiguration
+from conan.tools.scm import Version
 
 
 class ToolchainsConan(ConanFile):
@@ -12,35 +13,70 @@ class ToolchainsConan(ConanFile):
     homepage = "https://github.com/tttapa/toolchains"
     settings = "os", "arch"
 
-    options = {
-        "target": [
-            "x86_64-bionic-linux-gnu",
-            "aarch64-rpi3-linux-gnu",
-            "armv8-rpi3-linux-gnueabihf",
-            "armv7-neon-linux-gnueabihf",
-            "armv6-rpi-linux-gnueabihf",
-        ]
+    _arch_triplets = {
+        "x86_64": "x86_64-bionic-linux-gnu",
+        "armv8": "aarch64-rpi3-linux-gnu",
+        # "armv8-rpi3-linux-gnueabihf",
+        "armv7hf": "armv7-neon-linux-gnueabihf",
+        "armv6": "armv6-rpi-linux-gnueabihf",
     }
-    default_options = {"target": "x86_64-bionic-linux-gnu"}
+
+    def _get_gcc_version(self):
+        tgt_gcc_version = str(self.settings_target.compiler.version)
+        possible_versions = self.conan_data["supported-gcc-versions"][self.version]
+        if tgt_gcc_version in possible_versions:
+            return tgt_gcc_version
+        for p in possible_versions:
+            v = Version(p)
+            tgt_v = Version(tgt_gcc_version)
+            if (tgt_v.major or "") == v.major and (tgt_v.minor or v.minor) == v.minor:
+                return p
+
+        msg = f"Invalid GCC version '{tgt_gcc_version}'. "
+        msg += f"Only the following versions are supported for the compiler: {', '.join(possible_versions)}"
+        raise ConanInvalidConfiguration(msg)
+
+    @property
+    def _target_triplet(self):
+        return self._arch_triplets[str(self.settings_target.arch)]
 
     def validate(self):
         if self.settings.arch != "x86_64" or self.settings.os != "Linux":
-            msg = "Binaries are only provided for Linux on x86_64"
+            msg = f"This toolchain is not compatible with {self.settings.os}-{self.settings.arch}. "
+            msg += "It can only run on Linux-x86_64."
             raise ConanInvalidConfiguration(msg)
 
+        invalid_arch = str(self.settings_target.arch) not in self._arch_triplets
+        if self.settings_target.os != "Linux" or invalid_arch:
+            msg = f"This toolchain only supports building for Linux-{self._arch_triplets.join(',')}. "
+            msg += f"{self.settings_target.os}-{self.settings_target.arch} is not supported."
+            raise ConanInvalidConfiguration(msg)
+
+        if self.settings_target.compiler != "gcc":
+            msg = f"The compiler is set to '{self.settings_target.compiler}', but this "
+            msg += "toolchain only supports building with GCC."
+            raise ConanInvalidConfiguration(msg)
+
+        self._get_gcc_version()
+
     def package(self):
-        os_name = str(self.settings.os)
-        target = str(self.options.target)
+        ref = f"{self.settings.os}-{self.settings.arch}"
+        gcc_v = self._get_gcc_version()
+        tgt_ref = f"{self._target_triplet}-gcc{gcc_v}"
         get(
             self,
-            **self.conan_data["sources"][self.version][os_name][target],
+            **self.conan_data["sources"][self.version][ref][tgt_ref],
             destination=self.package_folder,
             strip_root=True,
         )
         os.system(f"chmod -R +w {self.package_folder}")
 
+    def package_id(self):
+        self.info.settings_target = self.settings_target
+        self.info.settings_target.rm_safe("build_type")
+
     def package_info(self):
-        target = str(self.options.target)
+        target = self._target_triplet
         bindir = os.path.join(self.package_folder, target, "bin")
         self.buildenv_info.prepend_path("PATH", bindir)
         libname = {
@@ -66,6 +102,7 @@ class ToolchainsConan(ConanFile):
             "c": f"{target}-gcc",
             "cpp": f"{target}-g++",
             "fortran": f"{target}-gfortran",
+            "asm": f"{target}-as",
         }
         self.conf_info.update("tools.build:compiler_executables", compilers)
         self.conf_info.define("tools.build.cross_building:cross_build", True)
